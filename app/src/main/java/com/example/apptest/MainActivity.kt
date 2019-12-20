91ba04d7461a026dcbc1e2e59afee85444219ed0
RomanaManuel <mr.romanamanuel@gmail.com>
2019-12-20 15:47:46 +0100

    add list item

--- app/src/main/java/com/example/apptest/MainActivity.kt
+++ app/src/main/java/com/example/apptest/MainActivity.kt
@@ -4,15 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+
 import android.widget.EditText
+import android.widget.ListView
+
 
 const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
+
 class MainActivity : AppCompatActivity() {
+    var array = arrayOf("Melbourne", "Vienna", "Vancouver", "Toronto", "Calgary", "Adelaide", "Perth", "Auckland", "Helsinki", "Hamburg", "Munich", "New York", "Sydney", "Paris", "Cape Town", "Barcelona", "London", "Bangkok")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val adapter = ArrayAdapter(this,
+            R.layout.listview_item, array)
+
+        val listView: ListView = findViewById(R.id.listview_1)
+        listView.setAdapter(adapter)
     }
+
 
     /** Called when the user taps the Send button */
     fun sendMessage(view: View) {
@@ -23,4 +36,7 @@ class MainActivity : AppCompatActivity() {
         }
         startActivity(intent)
     }
+
+
+
 }
